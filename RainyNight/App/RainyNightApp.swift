116a757ed123c855b716a03cdecb1d1bb82912f6
emilyj4482 <emilyj4482@gmail.com>
2026-05-06 18:59:11 +0900

    chore: create project

---
+++ RainyNight/App/RainyNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainyNightApp.swift
+//  RainyNight
+//
+//  Created by EMILY on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RainyNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ RainyNight/App/Root/RootView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RainyNight
+//
+//  Created by EMILY on 04/05/2026.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RootView()
+}
